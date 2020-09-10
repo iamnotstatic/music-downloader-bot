d0486ca53b11d87ab5a75af4d6536ac971b8f71a
Abdul-fatai <staticdev20046@gmail.com>
2020-09-10 01:51:37 +0100

    added help command

--- src/bot.js
+++ src/bot.js
@@ -2,17 +2,19 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.BOT_APIKEY, { polling: true });
 const axios = require('axios');
 
+// Process Music download
 bot.onText(/\/music (.+)/, (msg, match) => {
   let music = match[1];
   let chatId = msg.chat.id;
 
   // Get Data
   (async () => {
-    bot.sendMessage(chatId, '_Looking for _' + music + '...', {
-      parse_mode: 'Markdown',
-    });
-
     try {
+      // Set Laoding
+      bot.sendMessage(chatId, '_Looking for _' + music + '...', {
+        parse_mode: 'Markdown',
+      });
+
       const res = await axios.get(
         `http://youtube-scrape.herokuapp.com/api/search?q=${music}/`
       );
@@ -30,7 +32,6 @@ bot.onText(/\/music (.+)/, (msg, match) => {
   const getDownloadURL = (data) => {
     const size = 5;
     const limited = data.splice(0, size);
-    // const downloadURL = `https://cutepup.club/${url}`;
     limited.forEach((music) => {
       const {
         title,
@@ -39,10 +40,10 @@ bot.onText(/\/music (.+)/, (msg, match) => {
         thumbnail_src,
         views,
         upload_date,
-      } = music.video;
+      } = music && music.video;
 
       const options = {
-        caption: `\nTitle: ${title} \nDuration: ${duration} \nViews: ${views} \nRelease Date: ${upload_date} \n🚀 Download Here: https://cutepup.club/${url}`,
+        caption: `\nTitle: ${title} \nDuration: ${duration} \nViews: ${views} \nRelease Date: ${upload_date} \n \n🚀 Download Here: https://cutepup.club/${url} \n \nDownload your music with ease here http://t.me/musichive_bot`,
         reply_markup: JSON.stringify({
           inline_keyboard: [
             [{ text: 'Download', url: `https://cutepup.club/${url}` }],
@@ -53,4 +54,17 @@ bot.onText(/\/music (.+)/, (msg, match) => {
       bot.sendPhoto(chatId, thumbnail_src, options);
     });
   };
+});
+
+// Get instructions
+bot.onText(/\/help/, (msg, match) => {
+  let music = match[1];
+  let chatId = msg.chat.id;
+  bot.sendMessage(
+    chatId,
+    'INSTRUCTION \n \nTo download a music you have to use the /music command then the title eg. /music eminem godzilla. \n\nPlease be specific with the title.',
+    {
+      parse_mode: 'Markdown',
+    }
+  );
 });
