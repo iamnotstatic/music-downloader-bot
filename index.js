bbafaa7e8bd0a297e9e3610f2ecbd9153df6ad5b
Abdul-fatai <staticdev20046@gmail.com>
2020-09-11 13:13:49 +0100

    V1 ready to deploy

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const packageInfo = require('./package.json');
 require('./src/bot');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/', function (req, res) {
+  res.json({
+    message: 'Welcome to Music Downloader BOt',
+    link: 'http://t.me/musichive_bot',
+    version: packageInfo.version,
+  });
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
